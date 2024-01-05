58908b5ef7af3264f0994943cf263d6bb11a73b0
winnielinn <yaysa903@gmail.com>
2024-01-05 22:28:14 +0800

    feat: update intro.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://winninelin-tech.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -87,7 +87,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Intro',
+                label: 'Hello World',
                 to: '/docs/intro',
               },
             ],
